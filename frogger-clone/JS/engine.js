455dd4290a79b1357ba632a7e7609319d47199e2
Lane H <52760891+LHDev95@users.noreply.github.com>
2021-02-27 19:25:51 -0600

    Update engine.js

--- frogger-clone/JS/engine.js
+++ frogger-clone/JS/engine.js
@@ -107,12 +107,12 @@ var Engine = (function (global) {
          * for that particular row of the game level.
          */
         var rowImages = [
-            '/portfolio/frogger-clone/images/water-block.png',   // Top row is water
-            '/portfolio/frogger-clone/images/stone-block.png',   // Row 1 of 3 of stone
-            '/portfolio/frogger-clone/images/stone-block.png',   // Row 2 of 3 of stone
-            '/portfolio/frogger-clone/images/stone-block.png',   // Row 3 of 3 of stone
-            '/portfolio/frogger-clone/images/grass-block.png',   // Row 1 of 2 of grass
-            '/portfolio/frogger-clone/images/grass-block.png'    // Row 2 of 2 of grass
+            'https://lhdev95.github.io/portfolio/frogger-clone/images/water-block.png',   // Top row is water
+            'https://lhdev95.github.io/portfolio/frogger-clone/images/stone-block.png',   // Row 1 of 3 of stone
+            'https://lhdev95.github.io/portfolio/frogger-clone/images/stone-block.png',   // Row 2 of 3 of stone
+            'https://lhdev95.github.io/portfolio/frogger-clone/images/stone-block.png',   // Row 3 of 3 of stone
+            'https://lhdev95.github.io/portfolio/frogger-clone/images/grass-block.png',   // Row 1 of 2 of grass
+            'https://lhdev95.github.io/portfolio/frogger-clone/images/grass-block.png'    // Row 2 of 2 of grass
         ],
             numRows = 6,
             numCols = 5,
@@ -169,11 +169,11 @@ var Engine = (function (global) {
      * all of these images are properly loaded our game will start.
      */
     Resources.load([
-        '/portfolio/frogger-clone/images/stone-block.png',
-        '/portfolio/frogger-clone/images/water-block.png',
-        '/portfolio/frogger-clone/images/grass-block.png',
-        '/portfolio/frogger-clone/images/enemy-bug.png',
-        '/portfolio/frogger-clone/images/char-boy.png',
+        'https://lhdev95.github.io/portfolio/frogger-clone/images/stone-block.png',
+        'https://lhdev95.github.io/portfolio/frogger-clone/images/water-block.png',
+        'https://lhdev95.github.io/portfolio/frogger-clone/images/grass-block.png',
+        'https://lhdev95.github.io/portfolio/frogger-clone/images/enemy-bug.png',
+        'https://lhdev95.github.io/portfolio/frogger-clone/images/char-boy.png',
     ]);
     Resources.onReady(init);
 
